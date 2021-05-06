1c4a89a862c1c6eeb06db0665cc5b7b0c878784a
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-05-05 22:16:14 -0400

    Game layout created

--- app/src/main/java/com/iamspd/connect3/MainActivity.java
+++ app/src/main/java/com/iamspd/connect3/MainActivity.java
@@ -3,6 +3,8 @@ package com.iamspd.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.red);
+
+        counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
     }
 }
