1a78ad4800edaf0d001390e7ea143a07ed5f5943
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-05-06 19:22:31 -0400

    Logic initialized

--- app/src/main/java/com/iamspd/connect3/MainActivity.java
+++ app/src/main/java/com/iamspd/connect3/MainActivity.java
@@ -1,12 +1,17 @@
 package com.iamspd.connect3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    // variables
+    private int activePlayer;
+    private int[] gameState;
+    private int[][] winningPositions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +19,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void dropIn(View view){
+    public void dropIn(View view) {
+
+        // 0 = yellow, 1 = red
+        activePlayer = 0;
+
+        // 2 means not yet played
+        gameState = new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+        // winning positions
+        winningPositions = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}};
 
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1000f);
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setImageResource(R.drawable.red);
+        if (gameState[tappedCounter] == 2) {
 
-        counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+
+                activePlayer = 1;
+
+            } else {
+                counter.setImageResource(R.drawable.red);
+
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
+
+            for (int[] winnigPosition : winningPositions){
+                System.out.println(gameState[winnigPosition[0]]);
+            }
+        }
     }
 }
